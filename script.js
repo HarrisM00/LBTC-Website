d0fb402a1c787a8b28049ba0b55e668130d4c6f1
HarrisM00 <Harrismahmood014@gmail.com>
2024-06-18 09:58:03 -0400

    first review

---
+++ script.js
@@ -0,0 +1,9 @@
+const reviews = [
+    {
+      id: 1,
+      name: "John Doe",
+      rating: 5,
+      review: "Excellent product! I would highly recommend it.",
+      date: "2023-06-15"
+    },
+]
